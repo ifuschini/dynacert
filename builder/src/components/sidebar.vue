299220d00ea4b83a7b83b6655b00c93ec8170803
Idel Fuschini <idel.fuschini@double77.com>
2021-05-13 16:15:11 +0200

    Fix minor bugs

--- builder/src/components/sidebar.vue
+++ builder/src/components/sidebar.vue
@@ -2,10 +2,10 @@
     <div class="bg-light border-right" id="sidebar-wrapper" ref="sidebarWrapper">
       <div class="sidebar-heading">Menu </div>
       <div class="list-group list-group-flush">
-        <a href="#/surveyManager" class="list-group-item list-group-item-action bg-light">Survey Manager</a>
-        <a href="#/surveyMapper" class="list-group-item list-group-item-action bg-light">Survey Mapper</a>
-        <a href="#/surveyViewer" class="list-group-item list-group-item-action bg-light">Survey Viewer</a>
-        <a href="#/surveyCategory" class="list-group-item list-group-item-action bg-light">Survey Gategory</a>
+        <a href="#/surveyManager" class="list-group-item list-group-item-action bg-light">Builder</a>
+        <a href="#/surveyMapper" class="list-group-item list-group-item-action bg-light">Mapper</a>
+        <a href="#/surveyViewer" class="list-group-item list-group-item-action bg-light">Viewer</a>
+        <a href="#/surveyCategory" class="list-group-item list-group-item-action bg-light">Gategory</a>
       </div>
     </div>
 </template>
