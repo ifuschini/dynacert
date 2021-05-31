f2d5a4258f72423760b69cca4fb2e7150b8b9d7b
Idel Fuschini <idel.fuschini@double77.com>
2021-05-31 13:18:30 +0200

    Update conf.php
    delete unused parameter

--- dynacert-viewer/public/ws/confdyna/conf.php
+++ dynacert-viewer/public/ws/confdyna/conf.php
@@ -7,29 +7,7 @@
     $dbpwd = "dynacert";
     $dbname = "dynadb";
     cors();
-    $session_time = 10000;
     $tokenLength=32;
     $reCapUrl = "https://www.google.com/recaptcha/api/siteverify";
     $reCaptchaSecretV3 = '<secret key>';
-    $expiredLicenseDate = "2022-02-01";
-    $costumer= "";
-    $emailCostumer= "";
-    $configPdf='config.json';
-    $imagePath='./confdyna/images/';
-    $iconsPath = './confdyna/icons/';
-    $jsonPath = './confdyna/pdfJson/';
-    $pdfImagePath = './confdyna/pdfImages/';
-    $repositoryPath='../../../dynarepository/';
-    $tmpDir='../../../tmp/';
-    $hostUrl='https://'.$_SERVER['HTTP_HOST'].'/ws/download.php';
-    $admUrl='https://'.$_SERVER['HTTP_HOST'].'/#/adm/main';
-    $linkUrl='https://'.$_SERVER['HTTP_HOST'].'/#/';
-
-$listCat = json_decode('
-    [
-        {
-            "text": "All",
-            "value": "all"
-        }
-    ]
-    '); ?>
+?>
